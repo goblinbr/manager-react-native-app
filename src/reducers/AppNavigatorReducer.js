d0699d1e1d989332b7e52ce5cd593bddbb52b518
Rodrigo de Bona Sartor <rodrigo.goblin@gmail.com>
2017-03-07 12:12:02 -0300

    Removed log

--- src/reducers/AppNavigatorReducer.js
+++ src/reducers/AppNavigatorReducer.js
@@ -1,6 +1,5 @@
 import { AppNavigator } from '../AppWithNavigationState';
 
 export default (state, action) => {
-  console.log(action);
   return AppNavigator.router.getStateForAction(action, state) || state;
 };
